61731898ecbc202d8fe4d18e3af8ce7e648bbb0d
Rhea Garud <rg921@ic.ac.uk>
2023-02-04 23:28:17 +0000

    Added large text for total time gained/lost

--- hack_app/lib/history_page.dart
+++ hack_app/lib/history_page.dart
@@ -13,24 +13,35 @@ class HistoryPage extends StatelessWidget {
       title: _title,
       home: Scaffold(
         appBar: AppBar(title: const Text(_title)),
-        body: Column(children: const [NetTime(), JourneyTable()]),
+        body: Column(children: const [NetTimeText(), NetTimeValue(), JourneyTable()]),
       ),
     );
   }
 }
 
-class NetTime extends StatelessWidget {
-  const NetTime({super.key});
+class NetTimeText extends StatelessWidget {
+  const NetTimeText({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Text(
       'Total Time Gained: ',
-      style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 2.0),
+      style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1.0),
     );
   }
 }
 
+class NetTimeValue extends StatelessWidget {
+  const NetTimeValue({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '34 secs',
+      style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 4.0),
+    );
+  }
+}
 
 class JourneyTable extends StatelessWidget {
   const JourneyTable({super.key});
