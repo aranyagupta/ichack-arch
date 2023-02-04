69bc2f02ba5b2a1961edcca00eaac9cff3279798
Avighna Jha <avighnajha1703@gmail.com>
2023-02-04 18:35:03 +0000

    Friends base code

---
+++ hack_app/lib/friends.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class FriendsList extends StatefulWidget {
+  @override
+  State<FriendsList> createState() => _FriendsList();
+}
+
+class _FriendsList extends State<FriendsList> {
+  Widget build(BuildContext context) {
+    return Container(
+        child: Column(
+      children: [],
+    ));
+  }
+}
